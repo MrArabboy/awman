30b619f0551aa8c64cf80130078ee0c78a037f1b
MrArabboy <mr.arabboy@gmail.com>
2021-11-16 11:08:34 +0500

    issue fixed

--- apps/staff/context_processor.py
+++ apps/staff/context_processor.py
@@ -2,15 +2,15 @@ from .models import OrganizationType, RewardType, Organization, Reward
 
 
 def filter_processor(request):
-    min_date = Reward.objects.order_by("date_of_issue").first().date_of_issue or 1991
-    max_date = Reward.objects.order_by("-date_of_issue").first().date_of_issue or 2021
+    # min_date = Reward.objects.order_by("date_of_issue").first().date_of_issue or 1991
+    # max_date = Reward.objects.order_by("-date_of_issue").first().date_of_issue or 2021
     reward_types = RewardType.objects.all()
     organizations = Organization.objects.all()
     organization_types = OrganizationType.objects.all()
 
     return {
-        "min_date": min_date,
-        "max_date": max_date,
+        "min_date": 1991,
+        "max_date": 2021,
         "reward_types": reward_types,
         "organizations": organizations,
         "organization_types": organization_types,
